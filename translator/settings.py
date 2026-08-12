986336c82b702f6ca90eec374630c94fa54a23f9
Dawitay <dawitayelewoldesenbet@gmail.com>
2026-08-12 11:28:32 +0300

    Add Vercel deployment configuration

--- translator/settings.py
+++ translator/settings.py
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost', '*']
